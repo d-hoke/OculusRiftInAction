77f208d0b6204effecd9d9da4bed041d96403cc2
Brad Davis <bradd@amazon.com>
2014-11-07 17:14:18 -0800

    Working on rasterizer idea

--- examples/cpp/experimental/ScratchApp.cpp
+++ examples/cpp/experimental/ScratchApp.cpp
@@ -3,18 +3,29 @@
 
 static const glm::uvec2 WINDOW_SIZE(1280, 800);
 static const glm::ivec2 WINDOW_POS(100, 100);
-static const glm::uvec2 RES(128, 80);
+static const glm::uvec2 RES(WINDOW_SIZE.x / 8, WINDOW_SIZE.y / 8);
+static const glm::uvec2 RASTER_RES(RES.x * 2, RES.y * 2);
 
 using namespace oglplus;
 
-struct Screen {
-  uvec2 resolution;
-  TexturePtr texture;
-};
+typedef std::vector<vec3> vv3;
+void rasterise(vec3 vs[3], vv3 & out) {
+}
+
+
+uvec2 mapNdcToPixel(czonst vec3 & v, const uvec2 & res) {
+  vec2(v.x, v.y)
+}
+
+uvec2 mapNdcToPixel(const vec3 & v, const uvec2 & res) {
+  vec2(v.x, v.y)
+}
+
 
 struct RasterizerExample : public GlfwApp {
   ProgramPtr program;
   ShapeWrapperPtr shape;
+  TexturePtr tex;
   BufferPtr edge;
 
   virtual GLFWwindow * createRenderingTarget(glm::uvec2 & outSize, glm::ivec2 & outPosition) {
@@ -31,58 +42,56 @@ struct RasterizerExample : public GlfwApp {
       Resource::SHADERS_RASTERIZER_FS);
     
     shapes::Plane plane(Vec3f(1, 0, 0), Vec3f(0, 1, 0)); 
-
-    const std::initializer_list<const GLchar*> names = { "Position", "TexCoord" };
-
-    shape = ShapeWrapperPtr(new shapes::ShapeWrapper(names, plane, *program));
-    //vao = VertexArrayPtr(new VertexArray());
-    //vao->Bind();
-    //pos = BufferPtr(new Buffer());
-    //tex = BufferPtr(new Buffer());
-    edge = BufferPtr(new Buffer());
-    //ind = BufferPtr(new Buffer());
-
-    //{
-    std::vector<GLfloat> posv;
-    plane.Positions(posv);
-
-    //  pos->Bind(Buffer::Target::Array);
-    //  Buffer::Data(Buffer::Target::Array, posv);
-    //  VertexArrayAttrib(*program, oria::Layout::Attribute::Position)
-    //    .Setup<GLfloat>(3)
-    //    .Enable();
-
-    //  std::vector<GLfloat> texv;
-    //  plane.TexCoordinates(texv);
-
-    //  tex->Bind(Buffer::Target::Array);
-    //  Buffer::Data(Buffer::Target::Array, texv);
-    //  VertexArrayAttrib(*program, oria::Layout::Attribute::TexCoord0)
-    //    .Setup<GLfloat>(2)
-    //    .Enable();
-
-    std::vector<GLfloat> edgev;
-    for (int i = 0; i < posv.size(); i += 3) {
-      float x = posv[i] * RES.x / 2;
-      edgev.push_back(x);
-      float y = posv[i + 1] * RES.y / 2;
-      edgev.push_back(y);
+    shape = ShapeWrapperPtr(new shapes::ShapeWrapper({ "Position", "TexCoord" }, plane, *program));
+    {
+      std::vector<GLfloat> posv;
+      plane.Positions(posv);
+      std::vector<GLfloat> edgev;
+      for (int i = 0; i < posv.size(); i += 3) {
+        float x = posv[i] * RES.x / 2;
+        edgev.push_back(x);
+        float y = posv[i + 1] * RES.y / 2;
+        edgev.push_back(y);
+      }
+      edge = BufferPtr(new Buffer());
+      edge->Bind(Buffer::Target::Array);
+      Buffer::Data(Buffer::Target::Array, edgev);
     }
 
-    //instr.Draw(indInfo);
     shape->Use();
-
-    edge->Bind(Buffer::Target::Array);
-    Buffer::Data(Buffer::Target::Array, edgev);
     VertexArrayAttrib(*program, 4)
       .Setup<GLfloat>(2)
       .Enable();
     NoVertexArray().Bind();
+    buildTextureData();
+  }
+  
+  void buildTextureData() {
+    std::vector<uint8_t> data;
+    data.resize(RES.x * RES.y * 3);
+    memset(&data[0], 255, data.size());
+    for (size_t i = 0; i < data.size(); ++i) {
+      if (0 == i % 2) data[i] = 0;
+    }
+    
+    tex = TexturePtr(new Texture());
+    Context::Bound(Texture::Target::_2D, *tex)
+      .MinFilter(TextureMinFilter::Nearest)
+      .MagFilter(TextureMagFilter::Nearest)
+      .WrapS(TextureWrap::MirroredRepeat)
+      .WrapT(TextureWrap::MirroredRepeat)
+      .Image2D(0, PixelDataInternalFormat::RGBA, RES.x, RES.y, 0, PixelDataFormat::RGB, PixelDataType::UnsignedByte, &data[0]);
+//    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, 2, 2, 0, GL_RGB, GL_UNSIGNED_BYTE, pixels);
+    
+//    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, RES.X, RES.y, 0, GL_RGB, GL_UNSIGNED_BYTE, )
   }
 
   virtual void draw() {
-    Context::Clear().ColorBuffer();
-    oria::renderGeometry(shape, program);
+    Context::Clear().ColorBuffer().DepthBuffer();
+    oria::renderGeometry(shape, program, { [&]{
+      glActiveTexture(GL_TEXTURE0);
+      tex->Bind(Texture::Target::_2D);
+    } });
   }
 };
 
